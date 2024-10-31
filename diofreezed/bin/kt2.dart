6a7fa771c6ba76b01981a9eed41e909bc425cc3d
N3k0-T1N <119561041+N3k0-T1N@users.noreply.github.com>
2024-10-31 13:30:00 +0500

    Delete diofreezed directory

--- diofreezed/bin/kt2.dart
+++
@@ -1,7 +0,0 @@
-import 'api_provider.dart';
-
-void main(List<String> arguments) async {
-  final apiProvider = ApiProvider();
-  final carts = await apiProvider.getCarts();
-  print(carts);
-}
--- diofreezed/test/kt2_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:kt2/diofreezed.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
